99305a662ba0343e0332cefec0f25e4d57e95ce7
Erika Sankari <8eikochan@gmail.com>
2017-09-22 15:01:24 +0300

    List different trash categories to categories.js.

--- src/categories.js
+++ src/categories.js
@@ -1,6 +1,7 @@
 export default {
     categories: [
         {
+            id: 100,
             name: "Sekajäte",
             dos: [
                 "Laita roskiin",
@@ -13,7 +14,8 @@ export default {
             description: "Test description"
         },
         {
-            name: "Biojäte",
+            id: 101,
+            name: "Bio- ja puutarhajäte",
             dos: [
                 "Laita bioroskiin",
                 "kierrätä"
@@ -23,6 +25,125 @@ export default {
                 "Perunan kuoret on ruokaa"
             ],
             description: "Test description"
-        }
+        },
+        {
+            id: 102,
+            name: "Energiajäte",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 103,
+            name: "Paperi",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 104,
+            name: "Pahvi",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 105,
+            name: "Kartonki",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 106,
+            name: "Metalli",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 107,
+            name: "Lasi",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 108,
+            name: "Vaarallinen jäte",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 109,
+            name: "Sähkölaitteet (SER)",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 110,
+            name: "Paristot",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 111,
+            name: "Muovi",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 113,
+            name: "Tekstiili",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 114,
+            name: "Muu jäte",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 115,
+            name: "Akut",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 116,
+            name: "Lamput",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 117,
+            name: "Puu",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 118,
+            name: "Kyllästetty puu",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
+        {
+            id: 119,
+            name: "Rakennus- ja purkujäte",
+            dos: [],
+            donts: [],
+            description: "Test description"
+        },
     ]
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,7 +6,6 @@ import {createLogger} from 'redux-logger';
 let middleware = [thunk];
 
 if (__DEV__) {
-   // const reduxImmutableStateInvariant = require('redux-immutable-state-invariant')();
     const logger = createLogger({collapsed: true});
 
     middleware = [...middleware, logger];
