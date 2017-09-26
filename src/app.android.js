b376eafbc753a8df1230307f7a552d134648b67e
Erika Sankari <8eikochan@gmail.com>
2017-09-26 10:07:39 +0300

    Add colors, dimensions and strings files.

--- src/app.android.js
+++ src/app.android.js
@@ -5,22 +5,26 @@ import {Navigation} from 'react-native-navigation';
 import {registerScreens} from './screens';
 import configureStore from './store/configureStore';
 
+import * as colors from './res/colors.json';
+import * as dimensions from './res/dimensions.json';
+
 const store = configureStore();
 
 registerScreens(store, Provider);
 
 const navigatorStyle = {
-    statusBarColor: 'black',
+    statusBarColor: colors.primaryDark,
     statusBarTextColorScheme: 'light',
     navigationBarColor: 'black',
-    navBarBackgroundColor: '#0a0a0a',
-    navBarTextColor: 'white',
-    navBarButtonColor: 'white',
+    navBarBackgroundColor: colors.primary,
+    navBarTextColor: colors.textLight,
+    navBarButtonColor: colors.textLight,
     navBarHiddenOnScroll: true,
 
-    topTabTextColor: 'white',
-    selectedTopTabIndicatorColor: 'white',
-    selectedTopTabTextColor: 'green',
+    topTabTextColor: colors.textSecondary,
+    selectedTopTabTextColor: colors.textLight,
+    selectedTopTabIndicatorHeight: dimensions.indicatorHeight,
+    selectedTopTabIndicatorColor: colors.textLight,
     tabBarHidden: false,
     drawUnderTabBar: true
 };
